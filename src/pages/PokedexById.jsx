84146485e6e0d07d3b6870335b4379c5982412d4
Edward Yoris <edwardyoris@gmail.com>
2022-10-28 13:13:41 -0500

    ready

--- src/pages/PokedexById.jsx
+++ src/pages/PokedexById.jsx
@@ -20,6 +20,8 @@ const PokedexById = () => {
       })
   }, [])
 
+  console.log(Pokemon)
+
   if (hasError) {
     return <Pokemon404 />
   }
@@ -30,7 +32,53 @@ const PokedexById = () => {
         <img src={Pokemon?.sprites.other['official-artwork'].front_default} alt="" />
       </header>
       <section>
+        <h1>#{Pokemon?.id}</h1>
         <h2>{Pokemon?.name}</h2>
+        <h5>Height</h5>
+        <p>{Pokemon?.height}</p>
+        <h5>Weight</h5>
+        <p>{Pokemon?.weight}</p>
+      </section>
+      <section>
+        <h3>Type</h3>
+        <ul>
+          {
+              Pokemon?.types.map(type => (
+                <li key={type.slot}>{type.type.name}</li>
+            ))
+          }
+        </ul>
+        <h3>Abilities</h3>
+        <ul>
+          {
+              Pokemon?.abilities.map(ability => (
+                <li key={ability.slot}>{ability.ability.name}</li>
+            ))
+          }
+        </ul> 
+      </section>
+      <section>
+        <h3>Stats</h3>
+        <ul>
+                {
+                    Pokemon?.stats.map(stat => (
+                        <li key={stat.stat.name}>
+                            <span>{stat.stat.name}</span>
+                            <span>{stat.base_stat}</span>
+                        </li>
+                    ))
+                }
+            </ul>
+      </section>
+      <section>
+        <h3>Movements</h3>
+        <ul>
+          {
+              Pokemon?.moves.map(move => (
+                <li key={move.move.name}><span>{move.move.name}</span></li>
+            ))
+          }
+        </ul> 
       </section>
     </article>
   )
